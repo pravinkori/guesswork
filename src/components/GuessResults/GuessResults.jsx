1816802ea0f5d9ffc9f988c01ea392f25914332f
Pravin Kori <pravinkori@protonmail.ch>
2024-04-14 10:32:36 +0530

    feat: Add GuessResults component
    Add GuessResults component with two guess paragraphs for displaying guesses.

---
+++ src/components/GuessResults/GuessResults.jsx
@@ -0,0 +1,10 @@
+function GuessResults() {
+  return (
+    <div className="guess-results">
+      <p className="guess">FIRST</p>
+      <p className="guess">GUESS</p>
+    </div>
+  );
+}
+
+export default GuessResults;
