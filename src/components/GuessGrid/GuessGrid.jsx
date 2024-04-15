ff1ac572f0d2ee1614a95a20c5cebfcc32f33e51
Pravin Kori <pravinkori@protonmail.ch>
2024-04-15 18:53:52 +0530

    ✨ Add functionality to display guesses in GuessResults
    - Introduce state variable for guesses list
    - Implement handleSubmitGuess function to add new guess
    - Pass list of guess values to GuessResults component
    - Display guess values in GuessGrid component

--- src/components/GuessGrid/GuessGrid.jsx
+++ src/components/GuessGrid/GuessGrid.jsx
@@ -1,10 +1,17 @@
+import { range } from "../../utils";
+
+// GuessGrid component receives a value (a guess) and displays it
 function GuessGrid({ value }) {
   return (
+    // Container div for the GuessGrid component
     <div>
       <p className="guess">
-        {value.split("").map((letter, index) => (
-          <span key={index} className="cell">
-            {letter}
+        {/* Mapping over a range of numbers up to 5 */}
+        {range(5).map((num) => (
+          // Rendering a span for each number with the corresponding letter from the guess value
+          <span key={num} className="cell">
+            {/* Displaying the letter at index 'num' of the guess value */}
+            {value ? value[num] : undefined}
           </span>
         ))}
       </p>
